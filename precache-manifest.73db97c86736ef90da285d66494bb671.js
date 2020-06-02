457e4e18a9b24be3977a77fb809ed74c016c0277
Ted Jang <tedjang@Teds-MacBook-Pro.local>
2020-06-02 15:01:26 -0700

    Updates

---
+++ precache-manifest.73db97c86736ef90da285d66494bb671.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5447e2484b8c65ea803ebdc12de6c92",
+    "url": "/project-4-assessment/index.html"
+  },
+  {
+    "revision": "5e8447a26c36f3d6b4ab",
+    "url": "/project-4-assessment/static/css/main.2ddb3bd6.chunk.css"
+  },
+  {
+    "revision": "3357667a71712448bd75",
+    "url": "/project-4-assessment/static/js/2.2018531e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/project-4-assessment/static/js/2.2018531e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e8447a26c36f3d6b4ab",
+    "url": "/project-4-assessment/static/js/main.47d3a017.chunk.js"
+  },
+  {
+    "revision": "11176ca57580c991af7e",
+    "url": "/project-4-assessment/static/js/runtime-main.548f4f76.js"
+  }
+]);
